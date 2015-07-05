616605ab05d291db22f1e0ba9f1ae0f1fc3980cc
Greg <greg@flashtalking.com>
2015-07-05 17:44:15 +0100

    Autocomplete + a few bug fixes + styling changes

--- rg.js
+++ rg.js
@@ -9,6 +9,103 @@ riot.tag('rg-alert', '<div each="{ opts.alerts }" class="alert { type }"> <a cla
 		};
 	
 });
+riot.tag('rg-autocomplete', '<div class="container { open: opened }" riot-style="width: { width }"> <input type="{ opts.type || \'text\' }" name="textbox" placeholder="{ opts.placeholder }" onkeydown="{ handleKeys }" oninput="{ filterItems }" onfocus="{ filterItems }"> <div class="dropdown { open: opened }" show="{ opened }"> <div class="list"> <ul> <li each="{ filteredItems }" onclick="{ parent.select }" class="item { active: active }"> { text } </li> </ul> </div> </div> </div>', 'rg-autocomplete .container { position: relative; display: inline-block; cursor: pointer; } rg-autocomplete .container.open { -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-autocomplete input { font-size: 1em; padding: 10px; border: 1px solid #D3D3D3; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; outline: none; } rg-autocomplete .container.open input { } rg-autocomplete .dropdown { position: relative; background-color: white; border: 1px solid #D3D3D3; border-top: 0; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; overflow-y: auto; overflow-x: hidden; } rg-autocomplete .dropdown.open { -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-autocomplete ul,rg-autocomplete li { list-style: none; padding: 0; margin: 0; } rg-autocomplete li { padding: 10px; border-top: 1px solid #E8E8E8; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; } rg-autocomplete li:first-child { border-top: 0; } rg-autocomplete li:hover { background-color: #f3f3f3; } rg-autocomplete li.active,rg-autocomplete li:hover.active { background-color: #ededed; }', function(opts) {
+		var _this = this;
+		_this.opened = true;
+		_this.textbox.value = opts.value || '';
+
+		_this.filterItems = function () {
+			_this.filteredItems = opts.items.filter(function (item) {
+				item.active = false;
+				if (_this.textbox.value.length == 0 ||
+					item.text.toString().toLowerCase().indexOf(_this.textbox.value.toString().toLowerCase()) > -1) {
+					return true;
+				}
+			});
+			if (_this.filteredItems.length > 0) {
+				_this.opened = true;
+			}
+			if (opts.onfilter) {
+				opts.onfilter();
+			}
+			_this.update();
+		};
+
+		_this.handleKeys = function (e) {
+			var length = _this.filteredItems.length;
+			if (length > 0 && [13, 38, 40].indexOf(e.keyCode) > -1) {
+				e.preventDefault();
+
+				var activeIndex = null;
+				for (var i = 0; i < length; i++) {
+					var item = _this.filteredItems[i];
+					if (item.active) {
+						activeIndex = i;
+						break;
+					}
+				}
+
+				if (activeIndex != null) {
+					_this.filteredItems[activeIndex].active = false;
+				}
+
+				if (e.keyCode == 38) {
+
+					if (activeIndex == null || activeIndex == 0) {
+						_this.filteredItems[length - 1].active = true;
+					} else {
+						_this.filteredItems[activeIndex - 1].active = true;
+					}
+				} else if (e.keyCode == 40) {
+
+					if (activeIndex == null || activeIndex == length - 1) {
+						_this.filteredItems[0].active = true;
+					} else {
+						_this.filteredItems[activeIndex + 1].active = true;
+					}
+				} else if (e.keyCode == 13 && activeIndex != null) {
+					_this.select({ item: _this.filteredItems[activeIndex] });
+				}
+			}
+			return true;
+		};
+
+		_this.select = function (item) {
+			item = item.item;
+			if (opts.onselect) {
+				opts.onselect(item);
+			}
+			_this.textbox.value = item.text;
+			_this.opened = false;
+		};
+
+		_this.closeDropdown = function (e) {
+			if (!_this.root.contains(e.target)) {
+				if (opts.onclose && _this.opened) {
+					opts.onclose();
+				}
+				_this.opened = false;
+				_this.update();
+			}
+		};
+
+		_this.on('mount', function () {
+			document.addEventListener('click', _this.closeDropdown);
+			document.addEventListener('focus', _this.closeDropdown, true);
+			_this.width = _this.textbox.getBoundingClientRect().width + 'px';
+			var dd = _this.root.querySelector('.dropdown');
+			dd.style.width = _this.width;
+			dd.style.position = 'absolute';
+			_this.opened = opts.opened;
+			_this.update();
+		});
+
+		_this.on('unmount', function () {
+			document.removeEventListener('click', _this.closeDropdown);
+			document.removeEventListener('focus', _this.closeDropdown, true);
+		});
+	
+});
 riot.tag('rg-bubble', '<div class="context"> <div class="bubble { visible: visible }"> { text } </div> <div class="content" onmouseover="{ showBubble }" onmouseout="{ hideBubble }" onclick="{ toggleBubble }"> <yield></yield> </div> </div>', 'rg-bubble .context,rg-bubble .content { display: inline-block; position: relative; } rg-bubble .bubble { position: absolute; display: block; top: -27px; left: 50%; -webkit-transform: translate3d(-50%, 0, 0); transform: translate3d(-50%, 0, 0); padding: 5px 10px; background-color: rgba(0, 0, 0, 0.8); color: white; text-align: center; font-size: 12px; line-height: 1; white-space: nowrap; opacity: 0; transition: opacity 0.1s, top 0.1s; } rg-bubble .visible { top: -30px; opacity: 1; } rg-bubble .bubble:after { content: \'\'; position: absolute; display: block; bottom: -10px; left: 50%; -webkit-transform: translate3d(-50%, 0, 0); transform: translate3d(-50%, 0, 0); width: 0; height: 0; border: 5px solid rgba(0, 0, 0, 0); border-top-color: rgba(0, 0, 0, 0.9); }', function (opts) {
 
 	var _this = this;
@@ -108,7 +205,7 @@ riot.tag('rg-modal', '<div class="overlay" if="{ opts.modal.visible }" onclick="
 	};
 	
 });
-riot.tag('rg-select', '<div class="select { open: opened }" riot-style="width: { width }"> <div class="field { open: opened}" onclick="{ toggle }"> { fieldText || opts.placeholder } <span class="down-arrow">&#x25BE;</span> </div> <div class="dropdown" show="{ opened }"> <div class="filter"> <input type="text" name="filter" class="filter-box" placeholder="{ opts[\'filter-placeholder\'] || \'Filter\' }" oninput="{ filterItems }"> </div> <div class="list"> <ul> <li each="{ opts.options }" show="{ available }" onclick="{ parent.select }" class="item { selected: selected, disabled: disabled }"> { text } </li> </ul> </div> </div> </div>', 'rg-select .select { position: relative; display: inline-block; cursor: pointer; } rg-select .select.open { -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-select .field { padding: 10px; background-color: white; border: 1px solid #D3D3D3; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; } rg-select .down-arrow { float: right; } rg-select .dropdown { position: relative; width: 100%; background-color: white; border: 1px solid #D3D3D3; border-top: 0; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; } rg-select .select.open .dropdown { -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-select .filter-box { -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; width: 100%; padding: 10px; font-size: 0.9rem; border: 0; outline: none; color: #555; } rg-select ul,rg-select li { list-style: none; padding: 0; margin: 0; } rg-select li { padding: 10px; border-top: 1px solid #E8E8E8; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; } rg-select .selected { font-weight: bold; background-color: #f8f8f8; } rg-select li:hover { background-color: #f3f3f3; }', function(opts) {
+riot.tag('rg-select', '<div class="select { open: opened }" riot-style="width: { width }"> <div class="field { open: opened}" onclick="{ toggle }"> { fieldText || opts.placeholder } <span class="down-arrow">&#x25BE;</span> </div> <div class="dropdown" show="{ opened }"> <div class="filter"> <input type="text" name="filter" class="filter-box" placeholder="{ opts[\'filter-placeholder\'] || \'Filter\' }" oninput="{ filterItems }"> </div> <div class="list"> <ul> <li each="{ opts.options }" show="{ available }" onclick="{ parent.select }" class="item { selected: selected, disabled: disabled }"> { text } </li> </ul> </div> </div> </div>', 'rg-select .select { position: relative; display: inline-block; cursor: pointer; } rg-select .select.open { -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-select .field { padding: 10px; background-color: white; border: 1px solid #D3D3D3; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; line-height: normal; } rg-select .down-arrow { float: right; } rg-select .dropdown { position: relative; width: 100%; background-color: white; border: 1px solid #D3D3D3; border-top: 0; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; } rg-select .select.open .dropdown { -webkit-box-shadow: 0 2px 10px -4px #444; -moz-box-shadow: 0 2px 10px -4px #444; box-shadow: 0 2px 10px -4px #444; } rg-select .filter-box { -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box; width: 100%; padding: 10px; font-size: 0.9rem; border: 0; outline: none; color: #555; } rg-select ul,rg-select li { list-style: none; padding: 0; margin: 0; } rg-select li { padding: 10px; border-top: 1px solid #E8E8E8; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; } rg-select .selected { font-weight: bold; background-color: #f8f8f8; } rg-select li:hover { background-color: #f3f3f3; }', function(opts) {
 		var _this = this;
 		_this.opened = true;
 
