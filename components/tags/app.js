e92b43f00517c504e1fb899e812a866649cf7a0e
Greg <greg@flashtalking.com>
2015-08-02 13:13:12 +0100

    v1.9.0 - tags component

---
+++ components/tags/app.js
@@ -0,0 +1,18 @@
+riot.tag('demo-app', '<rg-tags items="{ countries }" tags="{ tags }" type="text" placeholder="Enter a country name"></rg-tags>', function(opts) {
+		var _this = this;
+		_this.countries = [{
+			text: 'England'
+		}, {
+			text: 'Scotland'
+		}, {
+			text: 'Wales'
+		}, {
+			text: 'Ireland'
+		}];
+
+		_this.tags = [{
+			text: 'America'
+		}];
+
+	
+});
